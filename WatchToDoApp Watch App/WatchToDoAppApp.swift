d750507632a403030ab7fc0ad2ce76d22d3e8086
t.shibuya <t.shibuya@sonicmoov.com>
2024-08-08 17:50:19 +0900

    Initial Commit

---
+++ WatchToDoApp Watch App/WatchToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchToDoAppApp.swift
+//  WatchToDoApp Watch App
+//
+//  Created by L_0019 on 2024/08/08.
+//
+
+import SwiftUI
+
+@main
+struct WatchToDoApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WatchToDoApp/WatchToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchToDoAppApp.swift
+//  WatchToDoApp
+//
+//  Created by L_0019 on 2024/08/08.
+//
+
+import SwiftUI
+
+@main
+struct WatchToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
